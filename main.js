f6213d115d740a96fd6569bdbe9551dd3741cca4
Ario Tamaddoni <ariozone6@gmail.com>
2018-09-04 10:16:37 -0700

    Array of objects created #1

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 
 var string = 'grumpy wizard make toxic brew for the evil queen and jack'
 myArray = string.split('')
-console.log(myArray)
+var char = {}
+for (var i = 0; i < myArray.length; i++) {
+  char[i] = myArray[i]
+}
+console.log(char)
+
+function eachChar () {
+  var char = document.createElement('p')
+  char.textContent = myArray[i]
+  return char
+}

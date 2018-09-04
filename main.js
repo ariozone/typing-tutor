3f1b8658b37f533e49553b49c8cf474de369aa11
Ario Tamaddoni <ariozone6@gmail.com>
2018-09-04 13:35:31 -0700

    a function defined to render all characters and appends them to the page. #1

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 
+var $h1 = document.createElement('h1')
+$h1.textContent = 'Typing Tutor'
+document.body.appendChild($h1)
 var string = 'grumpy wizard make toxic brew for the evil queen and jack'
-myArray = string.split('')
-var myChars = {}
-for (var i = 0; i < myArray.length; i++) {
-  myChars[i] = myArray[i]
+var charsArray = string.split('')
+var objArray = []
+for (var i = 0; i < charsArray.length; i++) {
+  var obj = {}
+  obj.letter = charsArray[i]
+  objArray.push(obj)
 }
-console.log(myChars)
+//console.log(objArray)
 
-function eachChar (i) {
-  var char = document.createElement('p')
-  char.textContent = myChar[i]
-  return char
+function eachChar (char) {
+  var $span = document.createElement('span')
+  $span.textContent = char
+  return $span
 }
+
+function renderString (allChars) {
+  var $p = document.createElement('p')
+  for (var i = 0; i < allChars.length; i++) {
+    $p.appendChild(eachChar(charsArray[i]))
+  }
+  return $p
+}
+var myStr = renderString(objArray)
+document.body.appendChild(myStr)
